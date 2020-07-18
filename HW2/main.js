ea07a351d576415e148e5fca8f2ff0eb393b506f
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-07-18 22:21:48 +0300

    Cycles and branching

---
+++ HW2/main.js
@@ -0,0 +1,17 @@
+let inputNumberN = Number.parseFloat(prompt("Please enter first number"));
+let inputNumberM = Number.parseFloat(prompt("Please enter second number"));
+const checkNumberN = !Number.isFinite(inputNumberN) || !Number.isFinite(inputNumberM);
+
+if (checkNumberN) alert("please enter integer numbers");
+
+const useEvenOrOdd  = confirm ("Need to skip even numbers?");
+
+let sum = 0;
+  for (let i = inputNumberN; i <= inputNumberM; i++) {
+    if (useEvenOrOdd && i % 2 === 0) continue;
+    else {
+    sum += i;
+    }
+  };
+console.log(`total: ${sum}`);
+    
