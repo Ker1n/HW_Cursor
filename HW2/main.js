17a8f32939472453cf28fbc8ed66c3ec3699d6d9
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-07-18 22:31:44 +0300

    fixed

--- HW2/main.js
+++ HW2/main.js
@@ -2,7 +2,7 @@ let inputNumberN = Number.parseFloat(prompt("Please enter first number"));
 let inputNumberM = Number.parseFloat(prompt("Please enter second number"));
 const checkNumberN = !Number.isFinite(inputNumberN) || !Number.isFinite(inputNumberM);
 
-if (checkNumberN) alert("please enter integer numbers");
+if (checkNumberN) alert("please enter integer numbers"), location.reload();
 
 const useEvenOrOdd  = confirm ("Need to skip even numbers?");
 
