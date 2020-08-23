0fa970417353cc9c7ae32ce04248827b7809287c
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-08-23 19:12:17 +0300

    Date, async and Promises

---
+++ HW11/main.js
@@ -0,0 +1,21 @@
+console.log("Waiting random chinese glyphs.....")
+const time = Date.now()
+
+function getRandomChinese(length) {
+    return new Promise((resolve, reject) => {
+        glyphs = "";
+        let i = 0;
+        if (length <= 0) reject();
+        while (i < length) {
+            setTimeout(() => {
+                glyphs += String.fromCharCode(Date.now().toString().substring(8));
+                if (glyphs.length === length) resolve(glyphs);
+            }, (i + 1) * 50);
+            i++;
+        };
+    }).then((glyphs) => console.log(glyphs + " за "+ (Date.now()- time +"ms")))
+      .catch(() => console.log("enter a number greater than 0 "))
+};
+
+getRandomChinese(4)
+
