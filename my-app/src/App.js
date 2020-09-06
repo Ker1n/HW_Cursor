fa129f2ce4146ba32fc30c94198aae7c0392a253
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-09-06 20:18:36 +0300

    react_hw1

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Posts from './Posts/posts'
+
+class App extends React.Component {
+  render() {
+      return (
+          <div>
+              <Posts />
+          </div>
+      )
+  }
+}
+
+
+export default App;
