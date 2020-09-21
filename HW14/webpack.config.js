76de199ec2964f0cad4853173bc205a486959499
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-09-21 10:23:51 +0300

    add webpack config

---
+++ HW14/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const ImportHttpWebpackPlugin = require('import-http/webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new ImportHttpWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'HW#14 Webpack'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|git)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img'
+                }
+            },          
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
